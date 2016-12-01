445abbb3ba84b46269a9194c307ac902c7818a5e
KEN <kilol@qq.com>
2016-12-01 17:14:49 +0800

    TEST

---
+++ README.md
@@ -0,0 +1,2 @@
+# 自动测试化脚手架：Karma + Mocha + Chai
+[![Coverage Status](https://travis-ci.org/sayll/Karma-Mocha.svg?branch=master)](https://travis-ci.org/sayll/Karma-Mocha)
---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+module.exports = function (config) {
+	config.set({
+		basePath     : '',
+		frameworks   : ['mocha'],
+		files        : ['test/*.js'],
+		exclude      : [],
+		preprocessors: {
+			'test/*.js': ['webpack']
+		},
+		reporters    : [ 'progress','coverage'],
+		port         : 9876,
+		colors       : true,
+		logLevel     : config.LOG_INFO,
+		autoWatch    : false,
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers     : ['PhantomJS'],
+		
+		webpack          : {
+			module: {
+				loaders: [{
+					test   : /\.js$/,
+					exclude: /node_modules/,
+					loader : 'babel-loader',
+					query  : {
+						presets: ['es2015'],
+						plugins: ['istanbul']
+					}
+				}]
+			}
+		},
+		webpackMiddleware: {
+			noInfo: true
+		},
+		coverageReporter : {
+			dir      : 'coverage',
+			reporters: [{
+				type  : 'json',
+				subdir: '.',
+				file  : 'coverage.json',
+			}, {
+				type  : 'lcov',
+				subdir: '.'
+			}, {
+				type: 'text-summary'
+			}]
+		},
+		singleRun        : true
+	})
+}
